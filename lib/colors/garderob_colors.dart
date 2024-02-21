5ed45caaf37b0ebfa563efc199312daeecb3bafc
HellsTomato <148013078+HellsTomato@users.noreply.github.com>
2024-02-21 15:32:12 +0300

    Test version 0

---
+++ lib/colors/garderob_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GarderobColors{
+  const GarderobColors._();
+
+  static const background = Color(0xFF10141E);
+  static const wbackground = Colors.white;
+
+}
---
+++ lib/resources/image_clother.dart
@@ -0,0 +1,7 @@
+class ImageClother {
+
+  static const String hat = "assets/test_image/Bucket-hat.png";
+  static const String tShort = "assets/test_image/T-Short.jpg";
+  static const String jeans = "assets/test_image/Slim-FitJeans.png";
+  static const String shoose = "assets/test_image/Sneaker001.jpeg";
+}
