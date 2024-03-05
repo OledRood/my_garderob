7f69fb6449826851e9d6c646934b8d84d3a2240b
renatsharaga <gutarov032@bk.ru>
2024-03-05 21:37:28 +0300

    Обновил secret key для django

--- backend/my_garderob/my_garderob/settings.py
+++ backend/my_garderob/my_garderob/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)-f^@h1%1c0@4@(-!d&7$=%-ab7_h#o+k_=6w$5v_j%#(&jwai'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
