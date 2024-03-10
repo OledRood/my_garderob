bd00dd94ba7a5763050af6ba4b12b2ee941c7c9d
renatsharaga <gutarov032@bk.ru>
2024-03-10 22:06:37 +0300

    Сделал регистрацию

--- backend/my_garderob/my_garderob/settings.py
+++ backend/my_garderob/my_garderob/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clothes.apps.ClothesConfig',
+    'files.apps.FilesConfig',
+    'users.apps.UsersConfig',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
--- backend/my_garderob/users/views.py
+++ backend/my_garderob/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            User.objects.create_user(
+                email=serializer.validated_data.get('email'),
+                username=serializer.validated_data.get('username'),
+                password=serializer.validated_data.get('password'),
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
