47c7742c38e223306e0b51ca34d9d5a77fb9f079
OledRood <96789787+OledRood@users.noreply.github.com>
2024-05-12 23:36:03 +0300

    Delete frontend/lib/resources directory

--- frontend/lib/resources/image_clother.dart
+++
@@ -1,7 +0,0 @@
-class ImageClother {
-
-  static const String hat = "assets/test_image/Bucket-hat.png";
-  static const String tShort = "assets/test_image/T-Short.jpg";
-  static const String jeans = "assets/test_image/Slim-FitJeans.png";
-  static const String shoose = "assets/test_image/Sneaker001.jpeg";
-}
